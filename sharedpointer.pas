b4f76a7fcac1ad00a1500ae35043979743e53ac3
Ian Hickson <ian@hixie.ch>
2025-11-17 23:28:49 +0000

    add support for casting a shared pointer, and for using shared pointers in hashtables (largely untested)

--- sharedpointer.pas
+++ sharedpointer.pas
@@ -5,14 +5,16 @@ unit sharedpointer;
 interface
 
 type
-   generic TSharedPointer <T: class> = record
+   generic TSharedPointer <T{: class}> = record // restore constraint when https://gitlab.com/freepascal.org/fpc/source/-/issues/41497 is fixed
    private
       type
          SelfT = specialize TSharedPointer<T>;
       var
          FPointer: T;
          FRefCount: PCardinal;
-      class procedure DecRef(var Self: SelfT); static;
+      class procedure IncRef(var Self: SelfT); static; inline;
+      class procedure DecRef(var Self: SelfT); static; inline;
+      function GetAssigned(): Boolean; inline;
    public
       class operator Initialize(var Self: SelfT);
       class operator Finalize(var Self: SelfT);
@@ -21,12 +23,59 @@ type
       class operator :=(const Source: T): SelfT;
       procedure Free();
       property Value: T read FPointer;
+      property Assigned: Boolean read GetAssigned;
+   end;
+   
+   // Troubleshooting: Make sure not to assign the same pointer to two
+   // different TSharedPointer instances. When creating the managed
+   // value, assign it directly to a TSharedPointer, rather than to an
+   // intermediate.
+
+generic function CastSharedPointer<OldT: class; NewT: class>(Source: specialize TSharedPointer<OldT>): specialize TSharedPointer<NewT>;
+
+type // for use in hashtables etc
+   generic SharedPointerUtils<T: class> = record
+      class function Equals(const A, B: specialize TSharedPointer<T>): Boolean; static; inline;
+      class function LessThan(const A, B: specialize TSharedPointer<T>): Boolean; static; inline; unimplemented;
+      class function GreaterThan(const A, B: specialize TSharedPointer<T>): Boolean; static; inline; unimplemented;
+      class function Compare(const A, B: specialize TSharedPointer<T>): Int64; static; inline; unimplemented;
    end;
 
 implementation
 
 uses
-   sysutils;
+   sysutils {$IFDEF VERBOSE}, exceptions {$ENDIF};
+
+class procedure TSharedPointer.IncRef(var Self: SelfT);
+begin
+   Assert(system.Assigned(Self.FRefCount) = system.Assigned(Self.FPointer), 'Invariant violation: refcount and pointer are inconsistent in IncRef');
+   if (system.Assigned(Self.FRefCount)) then
+   begin
+      InterlockedIncrement(Self.FRefCount^);
+      {$IFDEF VERBOSE} Writeln('IncRef ', HexStr(Pointer(Self.FPointer)), ' to ', Self.FRefCount^, ' at ', HexStr(Self.FRefCount)); {$ENDIF}
+   end;
+end;
+
+class procedure TSharedPointer.DecRef(var Self: SelfT);
+begin
+   {$IFDEF VERBOSE}
+   if (Assigned(Self.FRefCount)) then
+      Writeln('DecRef ', HexStr(Pointer(Self.FPointer)), ' to ', Self.FRefCount^ - 1, ' at ', HexStr(Self.FRefCount));
+   {$ENDIF}
+   if (system.Assigned(Self.FRefCount) and (InterlockedDecrement(Self.FRefCount^) = 0)) then
+   begin
+      FreeAndNil(Self.FPointer);
+      Dispose(Self.FRefCount);
+      Self.FRefCount := nil;
+   end;
+end;
+
+function TSharedPointer.GetAssigned(): Boolean;
+begin
+   Result := system.Assigned(FPointer);
+   Assert(Result = system.Assigned(FRefCount));
+   Assert((not Result) or (FRefCount^ > 0));
+end;
 
 class operator TSharedPointer.Initialize(var Self: SelfT);
 begin
@@ -41,17 +90,18 @@ end;
 
 class operator TSharedPointer.AddRef(var Self: SelfT);
 begin
-   Assert(Assigned(Self.FRefCount) = Assigned(Self.FPointer), 'Invariant violation: refcount and pointer are inconsistent in AddRef');
-   if (Assigned(Self.FRefCount)) then
-      InterlockedIncrement(Self.FRefCount^);
+   IncRef(Self);
 end;
 
 class operator TSharedPointer.Copy(constref Source: SelfT; var Destination: SelfT);
 begin
    DecRef(Destination);
-   Assert(Assigned(Source.FRefCount) = Assigned(Source.FPointer), 'Invariant violation: Source refcount and pointer are inconsistent');
-   if (Assigned(Source.FRefCount)) then
+   Assert(system.Assigned(Source.FRefCount) = system.Assigned(Source.FPointer), 'Invariant violation: Source refcount and pointer are inconsistent');
+   if (system.Assigned(Source.FRefCount)) then
+   begin
       InterlockedIncrement(Source.FRefCount^);
+      {$IFDEF VERBOSE} Writeln('IncRef ', HexStr(Pointer(Source.FPointer)), ' to ', Source.FRefCount^, ' at ', HexStr(Source.FRefCount), ' (copy)'); {$ENDIF}
+   end;
    Destination.FRefCount := Source.FRefCount;
    Destination.FPointer := Source.FPointer;
 end;
@@ -59,11 +109,13 @@ end;
 class operator TSharedPointer.:=(const Source: T): SelfT;
 begin
    DecRef(Result); // {BOGUS Warning: Function result variable of a managed type does not seem to be initialized}
-   if (Assigned(Source)) then
+   if (system.Assigned(Source)) then
    begin
       New(Result.FRefCount);
       Result.FRefCount^ := 1;
       Result.FPointer := Source;
+      {$IFDEF VERBOSE} Writeln('IncRef ', HexStr(Pointer(Result.FPointer)), ' to ', Result.FRefCount^, ' at ', HexStr(Result.FRefCount), ' (initial)'); {$ENDIF}
+      {$IFDEF VERBOSE} Writeln(GetStackTrace()); {$ENDIF}
    end
    else
    begin
@@ -72,21 +124,41 @@ begin
    end;
 end;
 
-class procedure TSharedPointer.DecRef(var Self: SelfT);
-begin
-   if (Assigned(Self.FRefCount) and (InterlockedDecrement(Self.FRefCount^) = 0)) then
-   begin
-      FreeAndNil(Self.FPointer);
-      Dispose(Self.FRefCount);
-      Self.FRefCount := nil;
-   end;
-end;
-
 procedure TSharedPointer.Free();
 begin
    DecRef(Self);
    FPointer := nil;
    FRefCount := nil;
+end;
+
+
+generic function CastSharedPointer<OldT; NewT>(Source: specialize TSharedPointer<OldT>): specialize TSharedPointer<NewT>;
+begin
+   specialize TSharedPointer<NewT>.DecRef(Result); {BOGUS Warning: Function result variable of a managed type does not seem to be initialized}
+   Result.FPointer := Source.FPointer;
+   Result.FRefCount := Source.FRefCount;
+   specialize TSharedPointer<NewT>.IncRef(Result);
+end;
+
+
+class function SharedPointerUtils.Equals(const A, B: specialize TSharedPointer<T>): Boolean;
+begin
+   Result := A.FPointer = B.FPointer;
+end;
+
+class function SharedPointerUtils.LessThan(const A, B: specialize TSharedPointer<T>): Boolean;
+begin
+   raise Exception.Create('unimplemented');
+end;
+
+class function SharedPointerUtils.GreaterThan(const A, B: specialize TSharedPointer<T>): Boolean;
+begin
+   raise Exception.Create('unimplemented');
+end;
+
+class function SharedPointerUtils.Compare(const A, B: specialize TSharedPointer<T>): Int64;
+begin
+   raise Exception.Create('unimplemented');
 end;
 
 end.
